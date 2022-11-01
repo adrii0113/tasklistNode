04eebe34bbfbe28600458cf104b5d5fd38254d41
adrii0113 <adrian190401@gmail.com>
2022-11-01 16:38:15 +0100

    delete and update routes

--- src/routes/taskroutes.js
+++ src/routes/taskroutes.js
@@ -25,7 +25,6 @@ router.get('/gettasks', async (req, res)=>{
     
 });
 
-
 //obetner una tarea en especifico en funcion de su titulo
 
 router.post('/task:tittle', async (req,res)=>{
@@ -35,7 +34,7 @@ router.post('/task:tittle', async (req,res)=>{
     }
 })
 
-
+//crear una tarea
 router.post('/newtask', async (req, res)=>{
 
     const taskifexist = await Task.findOne({ tittle: req.body.tittle });
@@ -48,11 +47,42 @@ router.post('/newtask', async (req, res)=>{
         const newTask = await Task.create(task)
         res.status(200).json(newTask);
     }
-    
-    
 
 })
 
+//eliminar una tarea
+router.delete('/deletetask', async (req, res) =>{
+
+    const taskifexist = await Task.findOne({ tittle: req.body.tittle });
+   
+    if (taskifexist) {
+        await Task.findByIdAndDelete(taskifexist.id)
+        res.status(200).send('Tarea eliminada con exito')
+    } else {res.status(500).json('La tarea no existe en la base de datos')}
+})
+
+
+
+//modificar la descripcion de una tarea especifica
+
+router.post('/updatetask', async (req, res ) =>{
+
+   
+
+    //find task for update
+    const tasktoupdate = await Task.findOne({ tittle: req.body.tittle });
+
+    //if exists update
+    if (tasktoupdate) {
+        await Task.findByIdAndUpdate({ _id : tasktoupdate.id}, { description : req.body.description});
+        // const taskupdated = await Task.findOne({ tittle: req.body.tittle }); 
+        res.status(200).json('Tarea modificada con exito');
+    }
+    res.status(500).json('La tarea no existe en la base de datos');
+})
+
+
+//mostrar la informacion de una tarea ya sea buscando mediante la descripccion o el titulo
 
 module.exports = router;
 
